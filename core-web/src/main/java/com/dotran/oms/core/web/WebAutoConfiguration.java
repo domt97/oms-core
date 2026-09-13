4d633d0a7a5d5910f037ae84da994383a1ae534d
dotran <d01682875097@gmail.com>
2026-09-13 15:00:56 +0700

    core: update build.gradle modules

--- core-web/src/main/java/com/dotran/oms/core/web/WebAutoConfiguration.java
+++ core-web/src/main/java/com/dotran/oms/core/web/WebAutoConfiguration.java
@@ -1,10 +1,7 @@
 package com.dotran.oms.core.web;
 
-import com.dotran.oms.core.web.advice.GlobalExceptionHandler;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.context.annotation.Import;
 
 @AutoConfiguration
-@Import(GlobalExceptionHandler.class)
 public class WebAutoConfiguration {
 }
